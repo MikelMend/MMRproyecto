9150c3cb078d819295d2bf54c605ee6da9a4dc91
Mikel Mendoza <mikelmendozarubio89@gmail.com>
2019-03-04 22:01:10 +0100

    prueba 3

--- Test/sumador/TestASumar.java
+++ Test/sumador/TestASumar.java
@@ -27,5 +27,14 @@ public class TestASumar {
 		ASumar sumi = new ASumar("12");
 		String resultado = sumi.mostrar();
 		assertEquals("1 + 2 = 3", resultado);
-  }
+        }
+	/**
+	 * si es negativo el espacio queda vacío
+	 */
+	@Test
+	public void testNegativo() {
+		ASumar sumi = new ASumar("-1");
+		String resultado = sumi.mostrar();
+		assertEquals("", resultado);
+		}
 }
